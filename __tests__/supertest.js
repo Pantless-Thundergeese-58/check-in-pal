1091576e2e115bf0d96cb84cbbf934f88846228b
Donald Twiford <donaldptwiford@gmail.com>
2023-06-13 20:03:53 -0700

    update uri in derver modules and created frist supertest

--- __tests__/supertest.js
+++ __tests__/supertest.js
@@ -1,15 +1,17 @@
 const request = require('supertest');
 
-const server = 'hyyp://localhost:3000';
+const server = 'hyyp://localhost:8080';
 
 describe('Route integration', () => {
   
+  //test to show that server is not being delivered with the get request
   describe('/', () => {
     describe('GET', () => {
-      return request(server)
-        .get('/')
-        .expect('Content-type', /html\/html/)
-        .expect(200);
+      it('response with 200 status and text/html content type', () => {
+        return request(server)
+          .get('/')
+          .expect(308);
+      })
     });
   });
 
