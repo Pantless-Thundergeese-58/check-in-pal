903442908a43c674802b173656c07903cad0feb0
Donald Twiford <donaldptwiford@gmail.com>
2023-06-14 18:31:52 -0700

    add conditional checks for login and singup to check for undefined entries

--- server/controller/checkoutController.js
+++ server/controller/checkoutController.js
@@ -6,27 +6,55 @@ const checkoutController = {};
 //define getAllTable data method
 checkoutController.getAllTableData = async (req, res, next) => {
  
+  const { time_unit, activity } = req.body
+
+  dayQuery = `tbd`;
+  weekQuery = `tbd`;
+  monthQuery = `tbd`;
+  yearQuery = `tbd`;
+
+  const timeTypes = {
+    day: dayQuery,
+    week: weekQuery,
+    month: monthQuery,
+    year: yearQuery,
+  }
+
+  //check data types
+  if (typeof time_unit !== 'string' || !timeTypes[time_unit]) {
+    return next({
+      log: 'Error in checkoutController.getAllTableData, time_unit datatype is incorrect',
+      message: { err : "time_unit is wrong datatype or is not an accepted time type" }
+    })
+  }
+
+  if(activity && typeof activity !== 'string'){
+    return next({
+      log: 'Error in checkoutController.getAllTableData, activity datatype is incorrect',
+      message: { err : "activity is wrong datatype" }
+    })
+  }
+
   //try and catch block
   try {
-    // SQL request to time_card table
-    const text = `
-    SELECT * FROM time_card
-    `;
+    //create query variable
+    const text = timeTypes[time_unit];
+
 // Execute the SQL query and store the result in the 'result' variable
-    const result = await db.query(text);
+    // const result = await db.query(text);
 
     // Create a property called 'timecard' on the res.locals object, assign it the rows property (which is an array of objects) of the result object on line 16
-    res.locals.timecard = result.rows;
+    // res.locals.timecard = result.rows;
     
     // Take the last ids endtime the first ids start time and subtract to get the total hours
-    const totalHours = res.locals.timecard[res.locals.timecard.length - 1].endtime - res.locals.timecard[0].starttime;
+    // const totalHours = res.locals.timecard[res.locals.timecard.length - 1].endtime - res.locals.timecard[0].starttime;
     //push totalHours variable into the end of the res.locals.timecard array
-    res.locals.timecard.push({ total: totalHours });
+    // res.locals.timecard.push({ total: totalHours });
 
     // console.log('totalHours is: ', totalHours);
     // console.log('res.locals.timecard is: ', res.locals.timecard);
     //continue to the next middleware function
-    next();
+    return next();
   }
   //catch error in checkoutController.getAllTableData middleware function
   catch(err) {
--- server/routes/checkout.js
+++ server/routes/checkout.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // Route that handles get request to the DB for all the data in the time_card table
 router.post('/', checkoutController.getAllTableData, (req, res) => {
   // Send the data retrieved from the get request as json to the frontend/client
-  res.status(200).json(res.locals.timecard);
+  res.status(200).json("test complete");
 });
 
 module.exports = router;
