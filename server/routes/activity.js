b917ca2961250ebc2f7955bfd4e8316012109c33
Donald Twiford <donaldptwiford@gmail.com>
2023-06-14 17:20:36 -0700

    set up activity and switch controllers to work with new database structure, change to test db

--- server/routes/activity.js
+++ server/routes/activity.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 // Route that handles patch request to the SQL database, updating the end time of an activity before switching to a new one
 router.patch('/', activityController.updateEndtime, (req, res) => {
-  res.sendStatus(200);
+  res.status(200).json("entry complete");
 });
 
 module.exports = router;
