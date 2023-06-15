af393cddad408b0d636809b89d22a6e90f659640
Donald Twiford <donaldptwiford@gmail.com>
2023-06-14 19:59:54 -0700

    set up query for getting data on days in checkout controller Co-authored-by: Shuai Shao shao-shuai@github.com

--- server/controller/activityController.js
+++ server/controller/activityController.js
@@ -16,15 +16,30 @@ activityController.updateEndtime = async (req, res, next) => {
   }
 
   // SQL query string for the patch request
-  const text = 
+  //create endtime query
+  const text1 = 
   `UPDATE time_card
-  SET endtime = $1, duration = (endtime - startime)
-  WHERE id = $2`;
+  SET endtime = $1
+  WHERE id = $2;`;
+
+  //create duration query
+  const text2=
+  `UPDATE time_card
+  SET duration = (endtime - startime)
+  WHERE id = ${timer_id}`;
     
   // Array params that dynamically assigns the value on line 15 for the SET part of the SQL query
   const params = [endTime, timer_id];
+
   // Store the result of the db query asynchronously in the result constant.
-  const result = await db.query(text, params)
+  const query1 = await db.query(text1, params)
+    .then(data => data)
+    .catch(err => next({
+      log: 'Error in activityController.updateEndtime when creating data entry',
+      message: { err: `${err}` }
+    }))
+
+  const query2 = await db.query(text2)
     .then(data => data)
     .catch(err => next({
       log: 'Error in activityController.updateEndtime when creating data entry',
--- server/controller/checkoutController.js
+++ server/controller/checkoutController.js
@@ -6,35 +6,65 @@ const checkoutController = {};
 //define getAllTable data method
 checkoutController.getAllTableData = async (req, res, next) => {
  
-  const { time_unit, activity } = req.body
+  const { user_id, time_unit, activity } = req.body
 
-  dayQuery = `tbd`;
-  weekQuery = `tbd`;
-  monthQuery = `tbd`;
-  yearQuery = `tbd`;
+  const dayQuery = //does not take in activity
+  `WITH day_table AS
+  (
+    SELECT
+    to_char(startime, 'Day') AS day,
+    startime,
+    user_id,
+    activity,
+    endtime,
+    EXTRACT(EPOCH FROM (endtime - startime)) / 60 AS duration_minutes,
+    duration
+    FROM time_card
+    WHERE startime >= DATE(now()) - interval '7 days' 
+    AND startime <= now() 
+    AND user_id = ${user_id}
+    -- 	AND activity = ''
+    )
+    
+    SELECT day,
+    activity,
+    sum(duration_minutes) AS duration
+    FROM day_table
+    GROUP BY day, activity;`;
+    
+    const weekQuery = `tbd`;
+    const monthQuery = `tbd`;
+    const yearQuery = `tbd`;
+    
+    const timeTypes = {
+      day: dayQuery,
+      week: weekQuery,
+      month: monthQuery,
+      year: yearQuery,
+    }
 
-  const timeTypes = {
-    day: dayQuery,
-    week: weekQuery,
-    month: monthQuery,
-    year: yearQuery,
-  }
-
-  //check data types
-  if (typeof time_unit !== 'string' || !timeTypes[time_unit]) {
-    return next({
-      log: 'Error in checkoutController.getAllTableData, time_unit datatype is incorrect',
-      message: { err : "time_unit is wrong datatype or is not an accepted time type" }
-    })
-  }
-
-  //if string is defined but is not a string return next to error
-  if(activity && typeof activity !== 'string'){
-    return next({
-      log: 'Error in checkoutController.getAllTableData, activity datatype is incorrect',
-      message: { err : "activity is wrong datatype" }
-    })
-  }
+    //check data types
+    if (typeof time_unit !== 'string' || !timeTypes[time_unit]) {
+      return next({
+        log: 'Error in checkoutController.getAllTableData, time_unit datatype is incorrect',
+        message: { err : "time_unit is wrong datatype or is not an accepted time type" }
+      })
+    }
+  
+    //if string is defined but is not a string return next to error
+    if(activity && typeof activity !== 'string'){
+      return next({
+        log: 'Error in checkoutController.getAllTableData, activity datatype is incorrect',
+        message: { err : "activity is wrong datatype" }
+      })
+    }
+  
+    if(typeof user_id !== 'number' || !user_id) {
+      return next({
+        log: 'Error in checkoutController.getAllTableData, username is incorrect',
+        message: { err : "username is wrong datatype or undefined" }
+      })
+    }
 
   //create query variable
   const text = timeTypes[time_unit];
